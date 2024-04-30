ef896af77379199587b0df07e0941a6a3e823bc1
Nickurama <rokdoombeast@gmail.com>
2024-04-30 16:40:44 +0100

    Added test case for map

--- src/test/SnakeGameTests/GameMapTests.java
+++ src/test/SnakeGameTests/GameMapTests.java
@@ -75,4 +75,54 @@ public class GameMapTests
 		// Assert
 		assertThrows(SnakeGameException.class, () -> new GameMap(mapRect, 'o'));
 	}
+
+	@Test
+	public void ShouldCollideWhenObjectLeavesMap()  throws GeometricException, GameEngineException, SnakeGameException
+	{
+		// Arrange
+		Rectangle mapRect = new Rectangle(new Point[]
+		{
+			new Point(3, 3),
+			new Point(3, 6),
+			new Point(12, 6),
+			new Point(12, 3)
+		});
+		GameMap map = new GameMap(mapRect, 'o');
+
+		Square colliderSq = new Square(new Point[] {
+			new Point(0, 0),
+			new Point(0, 1),
+			new Point(1, 1),
+			new Point(1, 0),
+		});
+		MockCollider collider0 = new MockCollider(colliderSq.moveCentroid(new Point(2, 2)));
+		MockCollider collider1 = new MockCollider(colliderSq.moveCentroid(new Point(2, 7)));
+		MockCollider collider2 = new MockCollider(colliderSq.moveCentroid(new Point(13, 7)));
+		MockCollider collider3 = new MockCollider(colliderSq.moveCentroid(new Point(13, 2)));
+
+		Scene sc = new Scene();
+		sc.add(map);
+		sc.add(collider0);
+		sc.add(collider1);
+		sc.add(collider2);
+		sc.add(collider3);
+
+		GameEngineFlags flags = new GameEngineFlags();
+		flags.setUpdateMethod(GameEngineFlags.UpdateMethod.CODE);
+		flags.setRasterized(true);
+		flags.setTextual(true);
+
+		GameEngine engine = GameEngine.getInstance();
+		engine.init(flags, sc);
+		engine.start();
+
+		// Act
+		engine.step();
+
+		// Assert
+		assertTrue(collider0.hasCollided());
+		assertTrue(collider1.hasCollided());
+		assertTrue(collider2.hasCollided());
+		assertTrue(collider3.hasCollided());
+	}
 }
